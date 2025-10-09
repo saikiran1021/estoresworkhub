bd4bfa703aae9938b8f6c38c5386ac108811ef50
Sai Kiran Kurapati <saikirankurapati57@gmail.com>
2025-10-09 10:47:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-557894671-b8aba",
+  "appId": "1:82357507243:web:3dfb46ec0fe368335a53ee",
+  "apiKey": "AIzaSyA-ZRKTvmTQdl2r4QqQJNMXvz6Z-utng2Q",
+  "authDomain": "studio-557894671-b8aba.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "82357507243"
+};
